3e3d6afc1ac5eca77e895072307e77b8cb90babb
Florian Wilhelm <florian.wilhelm02@sap.com>
2023-05-04 11:49:30 +0200

    npe

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        Integer x = null;
+        System.out.println(x.toString());
         SpringApplication.run(DemoApplication.class, args);
     }
 
